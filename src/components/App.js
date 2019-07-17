254a3d8f0d25141045cfb8fbee81316f84e295e2
SamTaub <staub91@gmail.com>
2019-07-17 14:27:19 -0400

    cleaned up files and components

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PreviewHome from './PreviewHome';
+
+const App = () => {
+  return <PreviewHome />;
+};
+
+export default App;
--- src/components/PreviewHome.js
+++ src/components/PreviewHome.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const PreviewHome = () => {
-
-    return (
-        <div className="jumbotron bg-transparent">
-            <h1 className="display-4 text-white text-center font-weight-bold">Unprecedented.</h1>
-            <h1 className="display-4 text-white text-center font-weight-bold">August 2019</h1>
-        </div>
-    );
-
+  return (
+    <div className="jumbotron bg-transparent">
+      <h1 className="display-4 text-center font-weight-bold">Unprecedented.</h1>
+      <h1 className="display-4 text-center font-weight-bold">August 2019</h1>
+    </div>
+  );
 };
 
 export default PreviewHome;
